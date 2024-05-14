01360dbd7df45c458b8a5c17eb6e9866897bba27
Radovan Aradjanin <radovan@Radovans-MacBook-Pro.local>
2024-05-14 15:33:19 +0200

    Commit all

---
+++ src/App.styled.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+export const Container=styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: aliceblue;
+  margin: 0 auto;
+  border:2px solid silver;
+  border-radius: 5%;
+  box-shadow: 40px 40px 25px black;
+
+
+`
+export const ContainerInputBtn=styled.div`
+    display: flex;
+    justify-content: center;
+    border-bottom: 2px solid silver;
+`
+export const ContainerPosts=styled.div`
+    display: flex;
+    justify-content: center;
+    font-weight: 700;
+    
+
+`
+export const ContainerPostBtn=styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 2px solid silver;
+`
+export const Input=styled.input`
+    flex: 0 0 auto;
+`
+export const Button=styled.button`
+    flex: 0 0 auto;
+`
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { Container,Input ,Button,ContainerPosts,ContainerInputBtn,ContainerPostBtn} from './App.styled'
+
 
 function App() {
+  const [posts,setPosts]=useState<{post:string,hour:number,minut:number}[]>([]);
+  const [inputText,setInputText]=useState<string>("");
+  function savePosts (){
+    const time=new Date();
+    const hours=time.getHours();
+    const minuts=time.getMinutes();
+      setPosts([...posts,{post:inputText,hour:hours,minut:minuts}]);
+  }
+  function deletPost(index:number){
+    const newArr:{post:string,hour:number,minut:number}[]=[];
+      for(var i=0;i<posts.length;i++){
+        if(index===i){
+          continue;
+
+        }
+        else {
+          newArr.push(posts[i]);
+        }
+      }
+      setPosts(newArr);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ContainerInputBtn>
+      <Input onChange={(e)=>setInputText(e.target.value)}></Input>
+      <Button onClick={savePosts}>post</Button>
+      </ContainerInputBtn>
+      {posts.map((post,index)=>(
+      <ContainerPostBtn>
+        <ContainerPosts>
+           <p>{post.post}-</p>
+           <p>{post.hour}:</p>
+           <p>{post.minut}h</p>
+        </ContainerPosts>
+        <Button onClick={()=>deletPost(index)}>cancel</Button>
+      </ContainerPostBtn>
+        
+      ))}
+    </Container>
+    
   );
 }
 
